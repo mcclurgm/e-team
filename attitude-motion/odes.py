ca9f1bfb646920dbe3a845f31177279cccc6890a
mcclurgm <mcgearboulder@gmail.com>
2018-04-28 22:03:59 -0500

    Cleaned up ODE test and added parameter

--- attitude-motion/odes.py
+++ attitude-motion/odes.py
@@ -8,18 +8,26 @@ print(('hello', 'there'))
 a = np.array([[0.],
                  [-5.66181428],
                  [ 9.80655   ]])
-print(a.item(1))
-def func(q, t, acceleration):
-    q1 = q[1] # q1' = q2
-    q2 = acceleration.item(1)    # q2' = a
-    # q2 = a.item(1)    # q2' = a
-    return [q1, q2]
-# def func(q, t):
-#     return [q[1], -2*q[0] - q[1]]
+v = np.array([[0.],
+                 [0.],
+                 [0.]])
+r = np.array([[0.],
+                 [0.],
+                 [0.]])
 
-# a_t = np.arange(0, 1.05, 0.05)
+def func(q, t, acceleration):
+    q1 = q[1]                 # q1' = q2
+    q2 = acceleration.item(1) # q2' = a
+    return [q1, q2]
+
+def vector_func(q, t, acceleration):
+    q1 = q[1]         # q1' = q2
+    q2 = acceleration # q2' = a
+    return [q1, q2]
+
 a_t = [0, 1]
 t = time.time()
-sol = integrate.odeint(func, [0, 0], a_t, args=(a,))
+# sol = integrate.odeint(func, [0, 0], a_t, args=(a,))
+sol = integrate.odeint(func, [r, v], a_t, args=(a,))
 print('Time to use ODE solver: {0}'.format(time.time() - t))
 print(sol)
